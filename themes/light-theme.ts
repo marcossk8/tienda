40de34e16a06ef46724a2268bf20fc053ad90607
marcos almendros <marcosdalmendros@gmail.com>
2023-03-07 18:37:13 -0300

    Theme provider config

---
+++ themes/light-theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    background: {
+      default: '#f4f4f5'
+    },
+    primary: {
+      main: '#2155CD',
+    },
+    secondary: {
+      main: "#a8a8a8",
+    },
+  }
+});
